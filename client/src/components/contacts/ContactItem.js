a4b77d3e5cd1da0d40034a9acc5903d7053ca50d
delinuxist <delinuxist@gmail.com>
2021-05-03 17:01:00 +0000

    contactItem component file

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,61 @@
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import ContactContext from "../../context/contact/contactContext";
+
+const ContactItem = ({ contact }) => {
+  const contactContext = useContext(ContactContext);
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
+
+  const { type, id, name, email, phone } = contact;
+
+  const OnDelete = () => {
+    deleteContact(id);
+    clearCurrent();
+  };
+
+  return (
+    <div className="card bg-light">
+      <h3 className="text-primary text-left">
+        {name}{" "}
+        <span
+          style={{ float: "right" }}
+          className={
+            "badge " +
+            (type === "professional" ? "badge-success " : "badge-primary ")
+          }
+        >
+          {type}
+        </span>
+      </h3>
+      <ul className="list">
+        {email && (
+          <li>
+            <i className="fas fa-envelope-open ">{email}</i>
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className="fas fa-phone ">{phone}</i>
+          </li>
+        )}
+      </ul>
+      <p>
+        <button
+          className="btn btn-dark btn-sm"
+          onClick={() => setCurrent(contact)}
+        >
+          Edit
+        </button>
+        <button className="btn btn-danger btn-sm" onClick={OnDelete}>
+          Delete
+        </button>
+      </p>
+    </div>
+  );
+};
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired,
+};
+
+export default ContactItem;
