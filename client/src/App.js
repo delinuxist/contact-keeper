99de0b757da3e7159e9d031fb6a7b9ab078c67fb
delinuxist <delinuxist@gmail.com>
2021-05-04 22:14:37 +0000

    imported register component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Register from "./components/auth/Register";
 
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
@@ -17,6 +18,7 @@ const App = () => {
             <Navbar />
             <div className="container">
               <Switch>
+                <Route exact path="/register" component={Register} />
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
               </Switch>
