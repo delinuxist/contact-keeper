c4d9f3dde53479fa466d3c382ae79f5086f3c8cf
delinuxist <delinuxist@gmail.com>
2021-05-05 22:17:26 +0000

    added localStorage and alert component

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,17 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alert from "./components/layouts/Alerts";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
 import AlertState from "./context/alert/AlertState";
+import setAuthToken from "./utils/setAuthToken";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
@@ -20,10 +27,11 @@ const App = () => {
             <Fragment>
               <Navbar />
               <div className="container">
+                <Alert />
                 <Switch>
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
-                  <Route exact path="/" component={Home} />
+                  <PrivateRoute exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
                 </Switch>
               </div>
